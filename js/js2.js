43aaec5defa598b1d418d127f7829504445e2cef
a1exss <porkkoa@gmail.com>
2022-06-24 21:11:59 +0300

    added gitignore

---
+++ js/js2.js
@@ -0,0 +1,25 @@
+"Use strict"
+let nameOne = Math.round((1.005 + Number.EPSILON) * 100) / 100;
+console.log(nameOne)
+
+let value = parseFloat("135.58px")
+console.log(value)
+
+let value2 = 58 + "hi"
+if (isNaN) {
+	console.log('nan:)')
+}
+
+console.log(Math.max(10, 58, 39, -150, 0))
+
+console.log(Math.floor(59.856))
+
+let text = 'фрилансер';
+let text2 = text.slice(5, 6);
+console.log(text2.toUpperCase())
+console.log(text.toUpperCase())
+
+let name = "фрилансер";
+let name2 = "фрилансер";
+console.log(name.slice(3, 6));
+console.log(name2.includes('лан', 1));
