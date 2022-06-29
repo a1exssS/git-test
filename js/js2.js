1f2debf51a6bdaec501b8c4717cd02d6484d0764
a1exss <porkkoa@gmail.com>
2022-06-29 20:14:55 +0300

    just doing my work

--- js/js2.js
+++ js/js2.js
@@ -1,4 +1,71 @@
 "Use strict"
+
+/*let userEyes = 'your eyes color';
+console.log(userEyes)
+
+let user = 'Даниил';
+let userName = 'Даниил';
+console.log(user)
+
+if (0) {
+	console.log('false');
+} else if (" ") {
+	console.log('true');
+}
+
+for (let name = 1; name < 10; name++) {
+	console.log(name);
+	if (name == 5) break
+}
+console.log(`иди нахуй for не работает ${name}`)
+
+
+for (let num = 0; num < 4; num++) {
+	for (let size = 0; size < 3; size++) {
+		if (size == 2) break;
+		console.log(size);
+	}
+}
+
+function showName() {
+	console.log(1);
+}
+setTimeout(showName, 0)
+console.log(2);
+
+'use strict'
+
+let showMessage;
+
+if (2 > 1) {
+	showMessage = function () {
+		console.log('hi');
+	};
+}
+showMessage();
+
+
+
+let userInfo = {
+	name: "vasia",
+	age: "30",
+	showInfo() {
+		console.log(`${this.name}`);
+	}
+}
+let userw = userInfo;
+userInfo = null;
+userw.showInfo();
+
+userInfo = {
+	name: "Vasia",
+	age: 30,
+}
+delete userInfo.name
+for (let key in userInfo) {
+	console.log(userInfo[key]);
+}
+
 let nameOne = Math.round((1.005 + Number.EPSILON) * 100) / 100;
 console.log(nameOne)
 
@@ -22,4 +89,4 @@ console.log(text.toUpperCase())
 let name = "фрилансер";
 let name2 = "фрилансер";
 console.log(name.slice(3, 6));
-console.log(name2.includes('лан', 1));
+console.log(name2.includes('лан', 1));                  */
